52bbad2215e18a9fe90ef5e9722fcc75496a451a
newCodeRunner_Mac <57590297+newCodeRunner@users.noreply.github.com>
2021-01-17 21:08:20 +0500

    added Toggle

--- lib/components/Toggle.svelte
+++ lib/components/Toggle.svelte
@@ -1,44 +1,45 @@
 <script>
-  import { getString, getColor, getTextSize, getHeight, getWidth } from '../js/helpers';
-  
-  let _class, _label, _textSize, _colorBg, _colorText, _height, _width;
-  $: _class = getString($$props.class);
-  $: _label = getString($$props.label, '');
-  $: _textSize = getTextSize($$props.size, 'md');
-  $: _colorBg = getColor($$props.colorBg, 'brand');
-  $: _colorText = getColor($$props.colorText, 'white');
-  $: _height = getHeight($$props.size, 'md');
-  $: _width = getWidth($$props.size, 'md');
+  import {
+    getString,
+    getColor,
+    getTextSize,
+    getHeight,
+    getWidth,
+  } from "../js/helpers";
 
-  const _id = `st-check-${new Date().getTime()}-${(Math.random() * 100)}`;
+  let _class, _label, _textSize, _color, _height, _width;
+  $: _class = getString($$props.class);
+  $: _label = getString($$props.label, "");
+  $: _textSize = getTextSize($$props.size, "md");
+  $: _color = getColor($$props.color, "primary");
+  $: _height = getHeight($$props.size, "md");
+  $: _width = getWidth($$props.size, "md");
+
+  const _id = `st-check-${new Date().getTime()}-${Math.random() * 100}`;
 
   export let checked = false;
 </script>
 
 <div class={_class}>
-  <input id={_id} type="checkbox" class="hidden" checked="{checked}" on:click={() => { checked = !checked; }}>
-  <label for={_id} class="inline-flex items-center text-{_colorText}">
-    <div class="p-2 {_height} {_width}">
-      <div tabindex="-1" class="h-full w-full relative border {_textSize} border-{_colorBg} bg-{_colorBg} focus:outline-none focus:border-{_colorText}" />
+  <label for={_id} class="inline-flex items-center cursor-pointer">
+    <input id={_id} type="checkbox" class="hidden" checked="{checked}" on:click={() => { checked = !checked; }}>
+    <div class="relative inline-flex items-center {_height} {_width}">
+      <div class="w-full -h-6 rounded-full shadow-inner {checked ? `bg-${_color}` : 'bg-gray-500'}"/>
+      <div class="st-toggle-handle absolute -w-3 -h-3 rounded-full shadow bg-gray-300" />
     </div>
-    <span class="{_textSize}">{_label}</span>
+    <span class="ml-2 {_textSize}">{_label}</span>
   </label>
 </div>
 
 <style>
-  input[type="checkbox"] + label div div:after {
-    color: transparent !important;
-  }
-  input[type="checkbox"] + label div div:after , input[type="checkbox"]:checked + label div div:after {
-    position: absolute;
-    width: fit-content;
-    height: fit-content;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    content: '\2713';
-  }
-  input[type="checkbox"]:checked + label div div:after {
-    color: currentColor !important;
-  }
+.st-toggle-handle {
+  top: 50%;
+  left: 0%;
+  transform: translateY(-50%);
+  transition: all 0.3s ease-in-out;
+}
+
+input[type="checkbox"]:checked + div div.st-toggle-handle {
+  transform: translate(75%, -50%);
+}
 </style>
