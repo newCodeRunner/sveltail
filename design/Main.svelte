d4f6d443a0222c2740b9d8be833b88bc6dc9082d
newCodeRunner <arsheikh02@gmail.com>
2021-05-25 13:01:40 +0500

    adding bulma

--- design/Main.svelte
+++ design/Main.svelte
@@ -13,6 +13,8 @@
   import '../lib/css/effects.css';
   import '../lib/css/base.css';
 
+  import '../lib/css/bulma.css';
+
   let _headerHeight = 0;
   let leftDrawer;
 
@@ -47,8 +49,83 @@
     Hello World
   </Drawer>
   <section class="w-screen overflow-auto" style="height: calc(100vh - {_headerHeight}px);">
-    <main class="w-full flex-wrap overflow-x-hidden">
-      Hello World
+    <main class="w-full flex-wrap overflow-x-hidden p-4">
+      <div class="block">
+        <button class="button is-large is-primary">Button</button>
+      </div>
+      <div class="block">
+        <a class="button">Anchor</a>
+        <button class="button">Button</button>
+        <input class="button" type="submit" value="Submit input">
+        <input class="button" type="reset" value="Reset input">
+      </div>
+      <div class="block">
+        <button class="button is-white">White</button>
+        <button class="button is-light">Light</button>
+        <button class="button is-dark">Dark</button>
+        <button class="button is-black">Black</button>
+        <button class="button is-text">Text</button>
+        <button class="button is-ghost">Ghost</button>
+      </div>
+      <div class="block">
+        <div class="buttons">
+          <button class="button is-primary">Primary</button>
+          <button class="button is-link">Link</button>
+        </div>
+        
+        <div class="buttons">
+          <button class="button is-info">Info</button>
+          <button class="button is-success">Success</button>
+          <button class="button is-warning">Warning</button>
+          <button class="button is-danger">Danger</button>
+        </div>
+
+        <div class="buttons">
+          <button class="button is-primary is-light">Primary</button>
+          <button class="button is-link is-light">Link</button>
+        </div>
+        
+        <div class="buttons">
+          <button class="button is-info is-light">Info</button>
+          <button class="button is-success is-light">Success</button>
+          <button class="button is-warning is-light">Warning</button>
+          <button class="button is-danger is-light">Danger</button>
+        </div>
+      </div>
+      <div class="block">
+        <button class="button is-small">Small</button>
+        <button class="button">Default</button>
+        <button class="button is-normal">Normal</button>
+        <button class="button is-medium">Medium</button>
+        <button class="button is-large">Large</button>
+      </div>
+      <div class="buttons are-medium">
+        <button class="button">All</button>
+        <button class="button">Medium</button>
+        <button class="button">Size</button>
+      </div>
+      <div class="buttons are-small">
+        <button class="button">Small</button>
+        <button class="button">Small</button>
+        <button class="button">Small</button>
+        <button class="button is-normal">Normal</button>
+        <button class="button">Small</button>
+      </div>
+      <div class="block">
+        <button class="button is-small is-fullwidth">Small</button>
+        <button class="button is-fullwidth">Normal</button>
+        <button class="button is-medium is-fullwidth">Medium</button>
+        <button class="button is-large is-fullwidth">Large</button>
+      </div>
+      <div class="block">
+        <button class="button is-loading">Loading</button>
+        <button class="button is-primary is-loading">Loading</button>
+        <button class="button is-link is-loading">Loading</button>
+        <button class="button is-info is-loading">Loading</button>
+        <button class="button is-success is-loading">Loading</button>
+        <button class="button is-warning is-loading">Loading</button>
+        <button class="button is-danger is-loading">Loading</button>
+      </div>
     </main>
   </section>
   <footer>
--- design/tailwind.config.js
+++ design/tailwind.config.js
@@ -5,20 +5,6 @@ module.exports = {
       md: '768px',
       lg: '1024px',
     },
-    extend: {
-      colors: {
-        light: '#ffffff',
-        dark: '#000000',
-        brand: '#009999',
-        accent: '#e6b89c',
-        primary: '#0496ff',
-        secondary: '#5eb1bf',
-        info: '#9cafb7',
-        success: '#90be6d',
-        warning: '#ff9f1c',
-        danger: '#d81159',
-      },
-    },
   },
   future: {
     removeDeprecatedGapUtilities: true,
